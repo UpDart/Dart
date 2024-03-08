4d22cad613665bfb997cb026cd1ce821dfe9701d
UpDart <162523639+UpDart@users.noreply.github.com>
2024-03-08 01:36:33 -0300

    Add files via upload

---
+++ initial.dart
@@ -0,0 +1,11 @@
+void main(){
+    var nomeVar = 'Thiago S. Borghardt [VAR]';
+    Object nomeObj = 'Thiago S. Borghardt [OBJECT]';
+    String nomeString = 'Thiago S. Borghardt [STRING]';
+    dynamic nomeDynamic = 'Thiago S. Borghardt [DYNAMIC]';
+    nomeDynamic = 2024;
+    print(nomeVar);
+    print(nomeObj);
+    print(nomeString);
+    print(nomeDynamic);
+}
