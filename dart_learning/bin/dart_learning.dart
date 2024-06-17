60d650749675088b6da906a828988aee1aee2e6c
UpDart <meckd.dart@gmail.com>
2024-06-17 07:47:09 -0300

    On branch main

---
+++ dart_learning/bin/dart_learning.dart
@@ -0,0 +1,7 @@
+import 'package:dart_learning/dart_learning.dart' as dart_learning;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_learning.calculate()}!');
+}
+
+// Este programa foi criado automaticamente quando iniciei um novo projeto em Dart no VSCODE.
